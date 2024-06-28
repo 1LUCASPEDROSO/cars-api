960d6277ed67f980bd442617c60c81470609ed4c
lucas pedroso <pedrosolucas1745@gmail.com>
2024-06-28 17:16:10 -0300

    create app

---
+++ apiCars/src/main/java/WsWork/example/apiCars/ApiCarsApplication.java
@@ -0,0 +1,13 @@
+package WsWork.example.apiCars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCarsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCarsApplication.class, args);
+	}
+
+}
---
+++ apiCars/src/test/java/WsWork/example/apiCars/ApiCarsApplicationTests.java
@@ -0,0 +1,13 @@
+package WsWork.example.apiCars;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCarsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
