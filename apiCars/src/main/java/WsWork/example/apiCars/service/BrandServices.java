b16c20a5625dc39cdb7be1ccdd33284a83a48b68
lucas pedroso <pedrosolucas1745@gmail.com>
2024-06-29 17:44:13 -0300

    update brandServices with all methods of CRUD

--- apiCars/src/main/java/WsWork/example/apiCars/service/BrandServices.java
+++ apiCars/src/main/java/WsWork/example/apiCars/service/BrandServices.java
@@ -5,6 +5,7 @@ import WsWork.example.apiCars.Repository.BrandRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class BrandServices {
@@ -17,4 +18,21 @@ public class BrandServices {
     public Brand create(Brand brand){
         return brandRepository.save(brand);
     };
+
+    public List<Brand> listAllBrands(){
+       return brandRepository.findAll();
+    }
+
+    public Brand updateBrand(Brand brand) {
+        Optional<Brand> existBrand = brandRepository.findById(brand.getId());
+        if (existBrand.isPresent()){
+            return brandRepository.save(brand);
+        }
+        else {
+            throw new RuntimeException("Brand not found with id: " + brand.getId());
+        }
+    }
+    public void deleteBrand(long id){
+        brandRepository.deleteById(id);
+    }
 }
