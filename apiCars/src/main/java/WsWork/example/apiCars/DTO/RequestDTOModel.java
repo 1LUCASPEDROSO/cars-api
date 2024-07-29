98366a73cc3124493b65155245e5a705652df050
lucas pedroso <pedrosolucas1745@gmail.com>
2024-07-28 23:58:04 -0300

    feat : new DTOResponse and fix RequestDTOModel

--- apiCars/src/main/java/WsWork/example/apiCars/DTO/RequestDTOModel.java
+++ apiCars/src/main/java/WsWork/example/apiCars/DTO/RequestDTOModel.java
@@ -2,5 +2,5 @@ package WsWork.example.apiCars.DTO;
 
 import java.math.BigDecimal;
 
-public record RequestDTOModel(long brand_id, String name, BigDecimal fipe_value) {
+public record RequestDTOModel(long id,long brand_id, String name, BigDecimal fipe_value) {
 }
---
+++ apiCars/src/main/java/WsWork/example/apiCars/DTO/ResponseDTOModel.java
@@ -0,0 +1,6 @@
+package WsWork.example.apiCars.DTO;
+
+import java.math.BigDecimal;
+
+public record ResponseDTOModel(long brand_id, String name, BigDecimal fipe_value) {
+}
