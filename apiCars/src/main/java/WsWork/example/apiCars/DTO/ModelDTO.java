de2ff6aa059d361b9cd455dd0cbbbacdf464a8cd
lucas pedroso <pedrosolucas1745@gmail.com>
2024-08-18 00:22:57 -0300

    refactor: rename RequestModelDTO to ModelDTO

---
+++ apiCars/src/main/java/WsWork/example/apiCars/DTO/ModelDTO.java
@@ -0,0 +1,6 @@
+package WsWork.example.apiCars.DTO;
+
+import java.math.BigDecimal;
+
+public record ModelDTO(long id, long brand_id, String name, BigDecimal fipe_value) {
+}
--- apiCars/src/main/java/WsWork/example/apiCars/DTO/RequestDTOModel.java
+++
@@ -1,6 +0,0 @@
-package WsWork.example.apiCars.DTO;
-
-import java.math.BigDecimal;
-
-public record RequestDTOModel(long id,long brand_id, String name, BigDecimal fipe_value) {
-}
